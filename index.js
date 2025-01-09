328a8fb1f464c5584e13666b245df8db73ed4c79
mc2282 <mconstantin8491@gmail.com>
2025-01-09 20:10:48 -0300

    -
    -

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server is running on http://localhost:${port}`);
+});
